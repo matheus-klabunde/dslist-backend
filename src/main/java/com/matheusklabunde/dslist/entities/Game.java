7481fc32a47b7bdf2a97f0967e0dd2fc808483cb
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-05-09 10:08:24 -0400

    Game seed

--- src/main/java/com/matheusklabunde/dslist/entities/Game.java
+++ src/main/java/com/matheusklabunde/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT") //instrução para que a jpa gere esse campo como um texto.
 	private String longDescription;
 	
 	public Game() {
