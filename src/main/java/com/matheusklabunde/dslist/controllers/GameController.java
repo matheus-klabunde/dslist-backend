19035947806bf6665440cc68832e5ef08446ff94
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-05-10 10:27:47 -0400

    Get game by id

--- src/main/java/com/matheusklabunde/dslist/controllers/GameController.java
+++ src/main/java/com/matheusklabunde/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matheusklabunde.dslist.dto.GameDTO;
 import com.matheusklabunde.dslist.dto.GameMinDTO;
 import com.matheusklabunde.dslist.services.GameService;
 
@@ -16,6 +18,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}") //para configurar como "GET" e a rota.
+	public GameDTO findById(@PathVariable long id){ // para que o id seja corresponde ao da requisição.
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 	
 	@GetMapping //para configurar como "GET".
 	public List<GameMinDTO> findAll(){
--- src/main/java/com/matheusklabunde/dslist/services/GameService.java
+++ src/main/java/com/matheusklabunde/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matheusklabunde.dslist.dto.GameDTO;
 import com.matheusklabunde.dslist.dto.GameMinDTO;
 import com.matheusklabunde.dslist.entities.Game;
 import com.matheusklabunde.dslist.repositories.GameRepository;
@@ -17,6 +19,17 @@ public class GameService {
 				//Injetando uma instâcia do GameRepository dentro do GameService.
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) //Boa pratica: para que a operação com o banco de dados aconteça ACID.
+									// readOnly = true -> Assegurando que nao irá fazer nenhuma operação de escrita.
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+		
+	}
+	
+	@Transactional(readOnly = true) //Boa pratica: para que a operação com o banco de dados aconteça ACID.
+									// readOnly = true -> Assegurando que nao irá fazer nenhuma operação de escrita.
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
